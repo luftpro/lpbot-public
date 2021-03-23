dbe06054e6cf279572b6705aba901f7ee8c3e90c
luftpro <bf2osmlforyou@gmail.com>
2021-03-24 02:33:41 +0600

    working loop+user mention

--- bot.py
+++ bot.py
@@ -18,6 +18,7 @@ hr = random.randint(6, 7)
 mnt = random.randint(10, 59)
 msg_time = '0' + str(hr) + ':' + str(mnt)
 
+
 @bot.event
 async def on_ready():
     for guild in bot.guilds:
@@ -27,18 +28,23 @@ async def on_ready():
         f'{bot.user} has connected to Discord!\n Guild: '
         f'{guild.name}(id: {guild.id})'
     )
-    print(msg_time)
 
 @bot.event
 async def on_message(message):
     if bot.user.mentioned_in(message):
-        await message.channel.send("дратути")
+        auth = message.author.id
+        if auth == 224809612426215425:
+            message.channel.send(f"<@!{auth}> gay")
+        else:
+            await message.channel.send(f"<@!{auth}> ку!")
 
 async def morning():
+    global msg_time, hr, mnt
     await bot.wait_until_ready()
     message_channel = bot.get_channel(chat_id)
+    print(f'Next message at: {msg_time}')
 
-    while not bot.is_closed:
+    while True:
         time_now = datetime.strftime(datetime.utcnow(),'%H:%M')
         if time_now == msg_time:
             await message_channel.send("Доброе утро!")
@@ -47,10 +53,10 @@ async def morning():
             hr = random.randint(6, 7)
             mnt = random.randint(10, 59)
             msg_time = '0' + str(hr) + ':' + str(mnt)
-            print(msg_time)
+            print(f'Next message at: {msg_time}')
         else:
             skip = 1
         await asyncio.sleep(skip)
 
-
+bot.loop.create_task(morning())
 bot.run(TOKEN)
